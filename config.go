6189b4145027f1776c81edaac3ab3624c3b3391a
leko <rexx0520@gmail.com>
2024-08-12 00:38:45 +0800

    feat: support more channels

---
+++ config.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Telegram struct {
+	ChatId	 string	 `yaml:"chat_id"`
+	BotToken string	 `yaml:"token"`
+	ThreadId string	 `yaml:"thread_id"`
+}
+
+type SubClient struct {
+	GotifyClientToken	string	 `yaml:"token"`
+	Telegram			Telegram `yaml:"telegram"`
+}
+
+// Config is user plugin configuration
+type Config struct {
+	Clients	   []SubClient	`yaml:"clients"`
+	GotifyHost string		`yaml:"gotify_host"`
+}
+
+// DefaultConfig implements plugin.Configurer
+func (c *Plugin) DefaultConfig() interface{} {
+	return &Config{
+		Clients: []SubClient{
+			SubClient{
+				GotifyClientToken: "ExampleToken",
+				Telegram: Telegram{
+					ChatId:	"-100123456789",
+					BotToken: "YourBotTokenHere",
+					ThreadId: "OptionalThreadIdHere"
+				},
+			},
+		},
+		GotifyHost: "ws://localhost:80",
+	}
+}
+
+// ValidateAndSetConfig implements plugin.Configurer
+func (c *Plugin) ValidateAndSetConfig(config interface{}) error {
+	newConfig := config.(*Config)
+
+	// Validate each SubClient in the Clients slice
+	for i, client := range newConfig.Clients {
+		if client.GotifyClientToken == "" {
+			return fmt.Errorf("gotify client token is required for client %d", i)
+		}
+		if client.Telegram.BotToken == "" {
+			return fmt.Errorf("telegram bot token is required for client %d", i)
+		}
+		if client.Telegram.ChatId == "" {
+			return fmt.Errorf("telegram chat id is required for client %d", i)
+		}
+	}
+	
+	c.config = newConfig
+	return nil
+}
--- plugin.go
+++ plugin.go
@@ -7,6 +7,7 @@ import (
     "net/http"
     "os"
     "time"
+    "html/template"
 
 	"github.com/gotify/plugin-api"
     "github.com/gorilla/websocket"
@@ -15,39 +16,37 @@ import (
 // GetGotifyPluginInfo returns gotify plugin info
 func GetGotifyPluginInfo() plugin.Info {
 	return plugin.Info{
-        Version: "1.0",
-        Author: "Anh Bui",
+        Version: "1.1",
+        Author: "Anh Bui & Leko",
 		Name: "Gotify 2 Telegram",
         Description: "Telegram message fowarder for gotify",
-		ModulePath: "https://github.com/anhbh310/gotify2telegram",
-
+		ModulePath: "https://github.com/lekoOwO/gotify2telegram",
 	}
 }
 
 // Plugin is the plugin instance
 type Plugin struct {
-    ws *websocket.Conn;
-    msgHandler plugin.MessageHandler;
-    chatid string;
-    telegram_bot_token string;
-    gotify_host string;
+    config      *Config
+    ws          map[int]*websocket.Conn
+    msgHandler  plugin.MessageHandler
 }
 
 type GotifyMessage struct {
-    Id uint32;
-    Appid uint32;
-    Message string;
-    Title string;
-    Priority uint32;
-    Date string;
+    Id          uint32
+    Appid       uint32
+    Message     string
+    Title       string
+    Priority    uint32
+    Date        string
 }
 
 type Payload struct {
-	ChatID string `json:"chat_id"`
-	Text   string `json:"text"`
+	ChatID   string `json:"chat_id"`
+	Text     string `json:"text"`
+    ThreadId string `json:"msg_thread_id"`
 }
 
-func (p *Plugin) send_msg_to_telegram(msg string) {
+func (p *Plugin) send_msg_to_telegram(msg string, bot_token string, chat_id string, thread_id string) {
     step_size := 4090
     sending_message := ""
     for i:=0; i<len(msg); i+=step_size {
@@ -58,9 +57,9 @@ func (p *Plugin) send_msg_to_telegram(msg string) {
 		}
 
         data := Payload{
-        // Fill struct
-            ChatID: p.chatid,
+            ChatID: chat_id,
             Text: sending_message,
+            ThreadId: thread_id,
         }
         payloadBytes, err := json.Marshal(data)
         if err != nil {
@@ -69,7 +68,7 @@ func (p *Plugin) send_msg_to_telegram(msg string) {
         }
         body := bytes.NewReader(payloadBytes)
         
-        req, err := http.NewRequest("POST", "https://api.telegram.org/bot"+ p.telegram_bot_token +"/sendMessage", body)
+        req, err := http.NewRequest("POST", "https://api.telegram.org/bot"+ bot_token +"/sendMessage", body)
         if err != nil {
             fmt.Println("Create request false")
             return
@@ -85,11 +84,11 @@ func (p *Plugin) send_msg_to_telegram(msg string) {
     }
 }
 
-func (p *Plugin) connect_websocket() {
+func (p *Plugin) connect_websocket(url string, subClientIndex int) {
     for {
-        ws, _, err := websocket.DefaultDialer.Dial(p.gotify_host, nil)
+        ws, _, err := websocket.DefaultDialer.Dial(p.config.GotifyHost, nil)
         if err == nil {
-            p.ws = ws
+            p.ws[subClientIndex] = ws
             break
         }
         fmt.Printf("Cannot connect to websocket: %v\n", err)
@@ -97,26 +96,39 @@ func (p *Plugin) connect_websocket() {
     }
 }
 
-func (p *Plugin) get_websocket_msg(url string, token string) {
-    p.gotify_host = url + "/stream?token=" + token
-    p.chatid = os.Getenv("TELEGRAM_CHAT_ID")
-    p.telegram_bot_token = os.Getenv("TELEGRAM_BOT_TOKEN")
+func format_telegram_message(msg *GotifyMessage) {
+    // HTML Should be escaped here
+    title := string(template.HTML("<b>" + template.HTMLEscapeString(msg.Title) + "</b>"))
+    return fmt.Sprintf(
+        "%s\n%s\n\nDate: %s", 
+        title, 
+        template.HTMLEscapeString(msg.Message), 
+        msg.Date
+    )
+}
 
-    go p.connect_websocket()
+func (p *Plugin) get_websocket_msg(url string, subClient *SubClient, subClientIndex int) {
+    ws_url := url + "/stream?token=" + subClient.GotifyClientToken
+    go p.connect_websocket(ws_url, subClientIndex)
 
     for {
         msg := &GotifyMessage{}
-        if p.ws == nil {
+        if p.ws[subClientIndex] == nil {
             time.Sleep(3)
             continue
         }
-        err := p.ws.ReadJSON(msg)
+        err := p.ws[subClientIndex].ReadJSON(msg)
         if err != nil {
             fmt.Printf("Error while reading websocket: %v\n", err)
-            p.connect_websocket()
+            p.connect_websocket(ws_url, subClientIndex)
             continue
         }
-        p.send_msg_to_telegram(msg.Date + "\n" + msg.Title + "\n\n" + msg.Message)
+        p.send_msg_to_telegram(
+            format_telegram_message(msg),
+            subClient.Telegram.BotToken,
+            subClient.Telegram.ChatId,
+            subClient.Telegram.ThreadId
+        )
     }
 }
 
@@ -127,26 +139,32 @@ func (p *Plugin) SetMessageHandler(h plugin.MessageHandler) {
 }
 
 func (p *Plugin) Enable() error {
-    go p.get_websocket_msg(os.Getenv("GOTIFY_HOST"), os.Getenv("GOTIFY_CLIENT_TOKEN"))
+    for i, subClient := range p.config.Clients {
+        go p.get_websocket_msg(
+            p.config.GotifyHost, 
+            subClient, i
+        )
+    }
     return nil
 }
 
 // Disable implements plugin.Plugin
 func (p *Plugin) Disable() error {
-    if p.ws != nil {
-        p.ws.Close()
+    for i, ws := range p.ws {
+        if ws != nil {
+            ws.Close()
+        }
     }
     return nil
 }
 
 // NewGotifyPluginInstance creates a plugin instance for a user context.
 func NewGotifyPluginInstance(ctx plugin.UserContext) plugin.Plugin {
-    return &Plugin{}
+    return &Plugin{
+        ws: make(map[int]*websocket.Conn)
+    }
 }
 
 func main() {
     panic("this should be built as go plugin")
-    // For testing
-    // p := &Plugin{nil, nil, "", "", ""}
-    // p.get_websocket_msg(os.Getenv("GOTIFY_HOST"), os.Getenv("GOTIFY_CLIENT_TOKEN"))
 }
